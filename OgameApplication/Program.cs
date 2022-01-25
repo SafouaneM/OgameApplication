eece1196bd667c13a3fe2d7dc0e8067a3d419b44
Safouane <safouane_m@hotmail.nl>
2022-01-25 01:18:07 +0100

    first commit
    first commit

---
+++ OgameApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OgameApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hey test on a macbook!");
+        }
+    }
+}
